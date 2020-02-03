7cca7862809875d11a692b1ede649ea723bd5a6d
FavouriteDragon <ninjadragon008@icloud.com>
2020-02-03 11:10:22 -0800

    lab 9

--- src/main/java/john/ibcs/lab9/AtkinsJLab9.java
+++ src/main/java/john/ibcs/lab9/AtkinsJLab9.java
@@ -1,7 +1,5 @@
 package main.java.john.ibcs.lab9;
 
-import javafx.util.Pair;
-
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
@@ -115,27 +113,72 @@ public class AtkinsJLab9 {
     }
 
     private static void interestRateCalculator() {
-        double startAmount, interestRate;
+        double startAmount, interestRate, endAmount;
+
+        System.out.println("Welcome to the interestinator 2000! Please input your starting balance.");
+        startAmount = handleInputs(input.next());
+
+        System.out.println("Please enter the percent interest rate you want as a whole number.");
+        interestRate = handleInputs(input.next());
+
+        endAmount = startAmount * Math.pow(1 + interestRate / 100, 30);
+        System.out.println("Your balance after 30 years is $" + endAmount + ".");
     }
 
     private static void superInterestRateCalculator() {
-
+        double startAmount, interestRate, numberOfCompounds, monthlyPayments, endAmount;
     }
 
-    private static void stackHeapVariableTest(String test) {
-        test = "Huh.";
+    private static void picturePattern() {
+        double height, width;
+
+        System.out.println("Welcome to the picture patterninator 2000!");
+        System.out.println("Please enter a height, then a width.");
+
+        height = handleInputs(input.next());
+        width = handleInputs(input.next());
+
+
+        for (int j = 0; j < height; j++) {
+            for (int i = 0; i < width; i++) {
+                //Top and bottom of the rectangle.
+                if (j == 0 || j == height - 1) {
+                    System.out.print(i > 0 ? " *" : "*");
+                }
+                else {
+                    if (height % 2 != 0) {
+                        //Width of the rectangle;
+                        if (i == 0)
+                            System.out.print("*");//i == 0 ? "*" : " *");
+                        else {
+                            if (i % 2 != 0)
+                                System.out.print(" 0");
+                            else System.out.print(" *");
+                        }
+                    }
+                    else {
+                        //Width of the rectangle;
+                        if (i == 0)
+                            System.out.print("*");//i == 0 ? "*" : " *");
+                        else
+                            System.out.print(" *");
+                    }
+
+                }
+            }
+            System.out.println();
+        }
     }
+
 
     private static void potShotsAtPi() {
-        long number = 0, totalDartsinCircle = 0;
+        long number, totalDartsinCircle = 0;
         double xPos, yPos, circleArea, piGuess;
-
-        List<Pair<Double, Double>> coordList = new ArrayList<>();
 
         System.out.println("How many dart throws do you want?");
         number = handleInputs(input.next());
-
         int i;
+
         for (i = 0; i < number; i++) {
             xPos = new Random().nextDouble();
             xPos = clip(xPos, 0, 1);
@@ -146,17 +189,18 @@ public class AtkinsJLab9 {
                 totalDartsinCircle++;
             }
         }
+
         circleArea = ((float) totalDartsinCircle / (float) i) * 4;
         piGuess = circleArea;
 
-        System.out.println(piGuess);
+        System.out.println("The area of your circle, based on " + number + " guesses, is " + piGuess + "cm^2.");
 
     }
 
 
     public static void main(String[] args) {
         int number = 0;
-        String test = "Help";
+
         while (number != -1) {
             System.out.println();
             System.out.println("Please choose a program, using numbers 1 to 8. Choose -1 to terminate the program.");
@@ -164,6 +208,10 @@ public class AtkinsJLab9 {
             System.out.println("1: Add up all numbers from 7 to 70,000.");
             System.out.println("2: Create a multiplication table!");
             System.out.println("3: Get any number in the fibonacci sequence!");
+            System.out.println("4: Basic interest rate calculator.");
+            System.out.println("5: Super interest rate calculator.");
+            System.out.println("6: Picture pattern 1.");
+            System.out.println("7: More picture patterns!");
             System.out.println("8: Potshots at Pi!");
             number = handleInputs(input.next());
             switch (number) {
@@ -176,13 +224,17 @@ public class AtkinsJLab9 {
                 case 3:
                     fibonacciSequence();
                     break;
+                case 4:
+                    interestRateCalculator();
+                    break;
+                case 5:
+                    superInterestRateCalculator();
+                    break;
+                case 6:
+                    picturePattern();
+                    break;
                 case 8:
                     potShotsAtPi();
-                    break;
-                case 0:
-                    //I think Mr. Tinling is wrong, ngl
-                    stackHeapVariableTest(test);
-                    System.out.println("HMMM: " + test);
                     break;
                 case -1:
                     System.out.print("Process terminated");
