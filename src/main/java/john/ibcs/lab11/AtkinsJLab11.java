d7d06abf22faa4d0724788292e86039113598d3c
FavouriteDragon <ninjadragon008@icloud.com>
2020-03-06 10:20:30 -0800

    lab 11 done

--- src/main/java/john/ibcs/lab11/AtkinsJLab11.java
+++ src/main/java/john/ibcs/lab11/AtkinsJLab11.java
@@ -1,93 +1,110 @@
 package main.java.john.ibcs.lab11;
 
 import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
-import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.layout.FlowPane;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
 import javafx.scene.paint.Paint;
 import javafx.scene.shape.Circle;
 import javafx.scene.shape.Line;
 import javafx.scene.shape.Rectangle;
+import javafx.scene.shape.Shape;
+import javafx.scene.text.Text;
+import javafx.scene.transform.Scale;
 import javafx.stage.Stage;
 
 public class AtkinsJLab11 extends Application {
-
     public static void main(String[] args) {
         launch(args);
     }
 
     @Override
-    public void start(Stage primaryStage) throws Exception {
-        //Creates the flow plane, scene, and buttons
-        FlowPane rootNode = new FlowPane();
-        Scene scene = new Scene(rootNode, 400, 400);
-        Button colourButton = new Button(), scaleButton = new Button();
-        Line line = new Line(100, 10, 10, 10);
-        Rectangle filledRect = new Rectangle(), emptyRect = new Rectangle();
-        Circle circle = new Circle();
+    public void start(Stage primaryStage) {
+        Scale scale;
+        Circle fillInCircle, transparentCircle;
+        Text text;
+        Line line;
+        Rectangle transparentRectangle, fillInRectangle;
+        Pane objectPane, buttonPane;
+        Button scaleBtn, colorBtn;
+        VBox root;
 
-        double circlePosX = scene.getHeight() / 2, circlePosY = scene.getHeight() / 2;
-        double scale = 1;
-        int[] colours = new int[3];
-        int state1;
-        colours[0] = colours[1] = colours[2] = 0;
+        primaryStage.setTitle("Color and Scale");
 
-        circle.setCenterX(circlePosX * scale);
-        circle.setCenterY(circlePosY * scale);
-        circle.setRadius(100 * scale);
-        circle.setFill(Color.TRANSPARENT);
+        scale = new Scale(1, 1);
 
-        rootNode.setAlignment(Pos.CENTER);
-        rootNode.layout();
-        primaryStage.setScene(scene);
+        fillInCircle = new Circle(35, 35, 23);
 
-        colourButton.setText("Colour");
-        colourButton.setMinSize(colourButton.getMinWidth() * scale, colourButton.getMinHeight() * scale);
-        colourButton.setTranslateX(scene.getWidth() / 2 - colourButton.getMinWidth());
-        colourButton.setTranslateY(150 * scale);
-        colourButton.setOnAction(event -> {
-            toggleColour(colours, Color.rgb(colours[0], colours[1], colours[2]));
-            circle.setStroke(Color.rgb(colours[0], colours[1], colours[2]));
-            line.setStroke(Color.rgb(colours[0], colours[1], colours[2]));
+        text = new Text(70, 50, "This is drawn on the canvas");
+
+        transparentCircle = new Circle(185, 185, 100);
+        transparentCircle.setFill(null);
+        transparentCircle.setStroke(Color.BLACK);
+
+        line = new Line(5, 5, 185, 185);
+
+        transparentRectangle = new Rectangle(20, 160, 40, 180);
+        transparentRectangle.setFill(null);
+        transparentRectangle.setStroke(Color.BLACK);
+
+        //225, 40
+        fillInRectangle = new Rectangle(100, 300, 275, 40);
+
+        objectPane = new Pane();
+        objectPane.getTransforms().add(scale);
+        objectPane.getChildren().addAll(fillInCircle, text, transparentCircle, line, transparentRectangle, fillInRectangle);
+
+        scaleBtn = new Button("Scale");
+        scaleBtn.setOnAction(event -> {
+            scale.setX(scale.getX() * 2);
+            scale.setY(scale.getY() * 2);
+            if (scale.getX() > 1) {
+                scale.setX(scale.getX() / 8);
+                scale.setY(scale.getY() / 8);
+            }
         });
 
-        scaleButton.setText("Scale");
-        scaleButton.setMinSize(colourButton.getMinWidth() * scale, colourButton.getMinHeight() * scale);
-        scaleButton.setTranslateX(scene.getWidth() / 2 - colourButton.getMinWidth());
-        scaleButton.setTranslateY(150 * scale);
+        scaleBtn.setLayoutX(150);
+        scaleBtn.setLayoutY(25);
 
-        //Event handlers
-        //Colours
+        colorBtn = new Button("Color");
+        colorBtn.setOnAction(event -> {
+            //figures out which color to fill
+            Paint currentColour = ((Shape) objectPane.getChildren().get(0)).getFill();
+            if (currentColour.equals(Color.BLACK))
+                currentColour = Color.RED;
+            else if (currentColour.equals(Color.RED))
+                currentColour = Color.BLUE;
+            else if (currentColour.equals(Color.BLUE))
+                currentColour = Color.GREEN;
+            else
+                currentColour = Color.BLACK;
 
 
+            int size = objectPane.getChildren().size();
+            for (int i = 0; i < size; i++) {
+                Shape object = (Shape) objectPane.getChildren().get(i);
+                if (object.getFill() == null)
+                    object.setStroke(currentColour);
+                else
+                    object.setFill(currentColour);
 
-        rootNode.getChildren().addAll(colourButton, scaleButton, circle);
+            }
+        });
+
+        colorBtn.setLayoutX(250);
+        colorBtn.setLayoutY(25);
+
+        buttonPane = new Pane();
+        buttonPane.getChildren().addAll(scaleBtn, colorBtn);
+
+        root = new VBox();
+        root.getChildren().addAll(objectPane, buttonPane);
+
+        primaryStage.setScene(new Scene(root, 400, 400));
         primaryStage.show();
-
-
-    }
-
-    private void toggleColour(int[] colours, Color initialColour) {
-      if (initialColour.getGreen() == 0 && initialColour.getBlue() == 0 && initialColour.getRed() == 0) {
-          colours[0] = 255;
-          colours[1] = colours[2] = 0;
-      }
-      else if (initialColour.getGreen() == 0 && initialColour.getBlue() == 0 && initialColour.getRed() == 1) {
-          colours[0] = 0;
-          colours[2] = 255;
-      }
-      else if (initialColour.getGreen() == 0 && initialColour.getBlue() == 1 && initialColour.getRed() == 0) {
-          colours[2] = 0;
-          colours[1] = 255;
-      }
-      else if (initialColour.getGreen() == 1 && initialColour.getBlue() == 0 && initialColour.getRed() == 0)
-          colours[0] = colours[1] = colours[2] = 0;
-      else colours[0] = colours[1] = colours[2] = 0;
-
     }
 
 }
